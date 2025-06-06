771d1d8dc6ff5ed06b187152a31a6c3f156e0890
Itxaka <itxaka@kairos.io>
2025-06-06 11:09:09 +0200

    Allow ksProvider to be empty
    Signed-off-by: Itxaka <itxaka@kairos.io>

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ import (
 var (
 	trusted      string
 	version      string
-	ksProvider   = newEnumFlag([]string{string(config.K3sProvider), string(config.K0sProvider)}, "")
+	ksProvider   = newEnumFlag([]string{string(config.K3sProvider), string(config.K0sProvider), ""}, "")
 	stageFlag    = newEnumFlag([]string{"init", "install", "all"}, "all")
 	loglevelFlag = newEnumFlag([]string{"debug", "info", "warn", "error", "trace"}, "info")
 )
