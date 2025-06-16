6bb33e884c16d01b9895331cded3340494bda3b5
Itxaka <itxaka@kairos.io>
2025-06-16 14:34:02 +0200

    Update pkg/stages/steps_init.go
    Co-authored-by: Mauro Morales <contact@mauromorales.com>

--- pkg/stages/steps_init.go
+++ pkg/stages/steps_init.go
@@ -332,7 +332,7 @@ func GetCleanupStage(sis values.System, l types.KairosLogger) []schema.Stage {
 		},
 		{
 			Name:     "Cleanup",
-			OnlyIfOs: "openSUSE.*|SUSE.*|[O-o]penSUSE.*",
+			OnlyIfOs: "openSUSE.*|SUSE.*",
 			Commands: []string{
 				"zypper clean -a",
 				"rm -rf /var/cache/zypp/* /tmp/* /var/tmp/*",
