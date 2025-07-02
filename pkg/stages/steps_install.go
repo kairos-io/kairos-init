c9f05c38d155e9b4d2b0280be2ac21d6f276763e
Itxaka <itxaka@kairos.io>
2025-07-02 11:14:48 +0200

    Drop epel
    Its not in the vanilla images, if users want they should add it
    themselves
    
    Signed-off-by: Itxaka <itxaka@kairos.io>

--- pkg/stages/steps_install.go
+++ pkg/stages/steps_install.go
@@ -47,17 +47,7 @@ func GetInstallStage(sis values.System, logger types.KairosLogger) ([]schema.Sta
 		return []schema.Stage{}, err
 	}
 
-	// TODO(rhel): Add zfs packages? Currently we add the repos to alma+rocky but we don't install the packages so?
 	stage := []schema.Stage{
-		{
-			Name:     "Install epel-release",
-			OnlyIfOs: "CentOS.*|Rocky.*|AlmaLinux.*",
-			Packages: schema.Packages{
-				Install: []string{
-					"epel-release",
-				},
-			},
-		},
 		{
 			Name: "Install base packages",
 			Packages: schema.Packages{
