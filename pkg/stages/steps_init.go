b9041f2638f06c985d4bc6c323d041b5da372670
Itxaka <itxaka@kairos.io>
2026-03-20 12:43:05 +0100

    feat: Add systemd-resolved to enabled services (#299)

--- pkg/stages/steps_init.go
+++ pkg/stages/steps_init.go
@@ -475,6 +475,7 @@ func GetServicesStage(_ values.System, l logger.KairosLogger) []schema.Stage {
 				Enable: []string{
 					"sshd",
 					"systemd-networkd",
+					"systemd-resolved",
 				},
 			},
 		},
